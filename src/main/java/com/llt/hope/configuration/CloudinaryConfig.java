c8afb044ae227234e3ba6a78ec084c1485797600
huutawn <huutankg2005@gmail.com>
2025-03-12 13:14:27 +0700

    cloudinary fix

--- src/main/java/com/llt/hope/configuration/CloudinaryConfig.java
+++ src/main/java/com/llt/hope/configuration/CloudinaryConfig.java
@@ -15,12 +15,10 @@ public class CloudinaryConfig {
 
     @Bean
     public Cloudinary cloudinary() {
-        Dotenv dotenv = Dotenv.load(); // Load biến môi trường từ .env
-
         Map<String, String> config = new HashMap<>();
-        config.put("cloud_name", dotenv.get("CLOUDINARY_CLOUD_NAME"));
-        config.put("api_key", dotenv.get("CLOUDINARY_API_KEY"));
-        config.put("api_secret", dotenv.get("CLOUDINARY_API_SECRET"));
+        config.put("cloud_name", System.getenv("CLOUDINARY_CLOUD_NAME"));
+        config.put("api_key", System.getenv("CLOUDINARY_API_KEY"));
+        config.put("api_secret", System.getenv("CLOUDINARY_API_SECRET"));
 
         return new Cloudinary(config);
     }
