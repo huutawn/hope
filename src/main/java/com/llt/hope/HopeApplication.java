ac396dcbc174a544434b4dd61bbc027ace976852
huutawn <huutankg2005@gmail.com>
2025-03-12 13:30:09 +0700

    server

--- src/main/java/com/llt/hope/HopeApplication.java
+++ src/main/java/com/llt/hope/HopeApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.llt.hope")
 public class HopeApplication {
 
     public static void main(String[] args) {
