fac221e76f0afc4a3981ff2a6099d16ea6d6fc68
nelhuynh <lenhuynh.lust.2803@gmail.com>
2025-04-02 01:35:46 +0700

    2/4/2025

--- src/main/java/com/llt/hope/repository/jpa/ProductRepository.java
+++ src/main/java/com/llt/hope/repository/jpa/ProductRepository.java
@@ -2,12 +2,19 @@ package com.llt.hope.repository.jpa;
 
 import java.util.Optional;
 
+import com.llt.hope.entity.Post;
+import com.llt.hope.entity.Seller;
+import com.llt.hope.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.llt.hope.entity.Product;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
     boolean existsByName(String name);
 
-    Optional<Product> findProductByName(String name);
+    Page<Product> findByNameContainingIgnoreCase(String name, Pageable pageable);
+    Page<Product> findProductBySeller(User seller, Pageable pageable);
 }
